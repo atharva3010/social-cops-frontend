823a07bf06e5ca4d2d06b2cc4cc670423496bff9
Atharva Sharma <mailatharvasharma@gmail.com>
2019-06-05 11:53:20 +0530

    updates

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -35,7 +35,7 @@
         </v-layout>
         <div id="about" style="padding:20px;">
           <div>
-            <div id="linkedin" class="LI-profile-badge hidden-sm-and-down"  data-version="v1" data-size="medium" data-locale="en_US" data-type="horizontal" data-theme="dark" data-vanity="atharva-sharma-802181162"><a class="LI-simple-link" href='https://in.linkedin.com/in/atharva-sharma-802181162?trk=profile-badge'>Atharva Sharma</a></div>
+            <div id="linkedin" class="LI-profile-badge"  data-version="v1" data-size="medium" data-locale="en_US" data-type="horizontal" data-theme="dark" data-vanity="atharva-sharma-802181162"><a class="LI-simple-link" href='https://in.linkedin.com/in/atharva-sharma-802181162?trk=profile-badge'></a></div>
             <h1 class="display-3">
               About Me: 
             </h1>
@@ -188,6 +188,10 @@ d3.csv('./sachin.csv')
     font-size: 60px;
     padding: 20px;
   }
+    #linkedin{
+    margin: 0;
+    width: 100%;
+  }
 }
 @media (max-width: 300px) {
   #header{
@@ -197,6 +201,10 @@ d3.csv('./sachin.csv')
     font-size: 50px;
     padding: 20px;
   }
+    #linkedin{
+    margin: 0;
+    width: 100%;
+  }
 }
 @media (max-width: 500px) {
   #header{
@@ -205,6 +213,10 @@ d3.csv('./sachin.csv')
   .typing{
     font-size: 60px;
     padding: 20px;
+  }
+  #linkedin{
+    margin: 0;
+    width: 100%;
   }
 }
 #about{
